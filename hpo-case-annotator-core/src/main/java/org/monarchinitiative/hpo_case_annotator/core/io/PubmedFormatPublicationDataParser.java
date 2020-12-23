cf53a348b7b4955b158befb39844b7f264fe7f22
Daniel Danis <daniel.danis@jax.org>
2020-12-23 11:39:51 -0500

    Implement parser for PubMed format

---
+++ hpo-case-annotator-core/src/main/java/org/monarchinitiative/hpo_case_annotator/core/io/PubmedFormatPublicationDataParser.java
@@ -0,0 +1,140 @@
+package org.monarchinitiative.hpo_case_annotator.core.io;
+
+import org.monarchinitiative.hpo_case_annotator.model.proto.Publication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * The class for parsing content such as shown <a href="https://pubmed.ncbi.nlm.nih.gov/33257779/?format=pubmed">here</a>
+ * into a {@link org.monarchinitiative.hpo_case_annotator.model.proto.Publication}.
+ */
+public class PubmedFormatPublicationDataParser {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PubmedFormatPublicationDataParser.class);
+
+    static Map<String, List<String>> preprocess(String payload) {
+        Pattern tagPattern = Pattern.compile("\\p{Upper}+\\s*-\\s*");
+        Matcher tagMatcher = tagPattern.matcher(payload);
+        List<Integer> begin = new ArrayList<>(), end = new ArrayList<>();
+        boolean isFirst = true;
+        while (tagMatcher.find()) {
+            if (isFirst) {
+                begin.add(tagMatcher.start());
+                isFirst = false;
+                continue;
+            }
+            begin.add(tagMatcher.start());
+            end.add(tagMatcher.start());
+        }
+        List<String> lines = new ArrayList<>();
+        for (int i = 0; i < end.size(); i++) {
+            String line = payload.substring(begin.get(i), end.get(i)).replaceAll("\\s{2,}", " ").replace('\n', ' ').trim();
+            lines.add(line);
+        }
+
+        Map<String, List<String>> values = new HashMap<>();
+        Pattern elementPattern = Pattern.compile("^(?<key>\\p{Upper}+)\\s*-\\s*(?<value>[\\p{Graph}\\p{Punct}\\p{Blank}]+)$");
+        for (String line : lines) {
+            Matcher elementMatcher = elementPattern.matcher(line);
+            if (!elementMatcher.matches()) {
+                LOGGER.warn("Unable to match line `{}`", line);
+                continue;
+            }
+            String key = elementMatcher.group("key");
+            String value = elementMatcher.group("value");
+            if (!values.containsKey(key)) {
+                values.put(key, new ArrayList<>());
+            }
+            values.get(key).add(value);
+        }
+        return values;
+    }
+
+    public Publication parse(String payload) throws PubMedParseException {
+        Publication.Builder builder = Publication.newBuilder();
+
+        Map<String, List<String>> elements = preprocess(payload);
+
+        // author list
+        if (elements.containsKey("AU")) {
+            builder.setAuthorList(String.join(", ", elements.get("AU")));
+        } else {
+            throw new PubMedParseException("No author found");
+        }
+
+        // title
+        if (elements.containsKey("TI")) {
+            builder.setTitle(elements.get("TI").get(0));
+        } else {
+            throw new PubMedParseException("Title not found");
+        }
+
+        // journal
+        if (elements.containsKey("TA")) {
+            builder.setJournal(elements.get("TA").get(0));
+        } else {
+            throw new PubMedParseException("Journal not found");
+        }
+
+        // year
+        if (elements.containsKey("DP")) {
+            String dateString = elements.get("DP").get(0);
+
+            Set<DateTimeFormatter> formatters = new HashSet<>();
+            formatters.add(DateTimeFormatter.ofPattern("yyyy MMM dd"));
+            formatters.add(DateTimeFormatter.ofPattern("yyyy MMM"));
+
+            String year = null;
+            for (DateTimeFormatter formatter : formatters) {
+                try {
+                    year = String.valueOf(LocalDate.parse(dateString, formatter).getYear());
+                    break;
+                } catch (DateTimeParseException ignored) {
+                }
+            }
+            if (year == null) {
+                if (LOGGER.isDebugEnabled()) {
+                    LOGGER.debug("Trying to perform naive parse of date string `{}`", dateString);
+                }
+                String yearString = dateString.substring(0, 4);
+                if (yearString.matches("\\d{4}")) {
+                    year = yearString;
+                } else {
+                    throw new PubMedParseException("Date not found");
+                }
+            }
+            builder.setYear(year);
+        }
+
+        // volume
+        String volume = "N/A";
+        if (elements.containsKey("VI") && elements.containsKey("IP")) {
+            volume = elements.get("VI").get(0) + '(' + elements.get("IP").get(0) + ')';
+        }
+        builder.setVolume(volume);
+
+        // pages
+        String pages = null;
+        if (elements.containsKey("PG")) {
+            pages = elements.get("PG").get(0);
+        }
+        builder.setPages(pages == null ? "N/A" : pages);
+
+        // PMID
+        if (elements.containsKey("PMID")) {
+            builder.setPmid(elements.get("PMID").get(0));
+        } else {
+            throw new PubMedParseException("PMID not found");
+        }
+
+        return builder.build();
+    }
+
+}
---
+++ hpo-case-annotator-core/src/test/java/org/monarchinitiative/hpo_case_annotator/core/io/PubmedFormatPublicationDataParserTest.java
@@ -0,0 +1,61 @@
+package org.monarchinitiative.hpo_case_annotator.core.io;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.monarchinitiative.hpo_case_annotator.model.proto.Publication;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.Assert.assertThat;
+
+public class PubmedFormatPublicationDataParserTest {
+
+    private PubmedFormatPublicationDataParser parser;
+
+    private static String readFile(Path path) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        parser = new PubmedFormatPublicationDataParser();
+    }
+
+    @Test
+    public void parse_epubAheadOfPrint() throws Exception {
+        String payload = readFile(Paths.get("src/test/resources/org/monarchinitiative/hpo_case_annotator/core/io/pubmed_fmt_epub.txt"));
+        Publication publication = parser.parse(payload);
+
+        assertThat(publication, hasProperty("authorList", equalTo("Pauper M, Kucuk E, Wenger AM, Chakraborty S, Baybayan P, Kwint M, van der Sanden B, Nelen MR, Derks R, Brunner HG, Hoischen A, Vissers LELM, Gilissen C")));
+        assertThat(publication, hasProperty("title", equalTo("Long-read trio sequencing of individuals with unsolved intellectual disability.")));
+        assertThat(publication, hasProperty("journal", equalTo("Eur J Hum Genet")));
+        assertThat(publication, hasProperty("year", equalTo("2020")));
+        assertThat(publication, hasProperty("pages", equalTo("N/A")));
+        assertThat(publication, hasProperty("volume", equalTo("N/A")));
+        assertThat(publication, hasProperty("pmid", equalTo("33257779")));
+
+    }
+
+    @Test
+    public void parse_fullPublicationRecord() throws Exception {
+        String payload = readFile(Paths.get("src/test/resources/org/monarchinitiative/hpo_case_annotator/core/io/pubmed_fmt_full.txt"));
+        Publication publication = parser.parse(payload);
+
+        assertThat(publication, hasProperty("authorList", equalTo("Niiranen TJ, Vasan RS")));
+        assertThat(publication, hasProperty("title", equalTo("Epidemiology of cardiovascular disease: recent novel outlooks on risk factors and clinical approaches.")));
+        assertThat(publication, hasProperty("journal", equalTo("Expert Rev Cardiovasc Ther")));
+        assertThat(publication, hasProperty("year", equalTo("2016")));
+        assertThat(publication, hasProperty("pages", equalTo("855-69")));
+        assertThat(publication, hasProperty("volume", equalTo("14(7)")));
+        assertThat(publication, hasProperty("pmid", equalTo("27057779")));
+    }
+}
