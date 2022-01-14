310c0fecbde68849e53d6b7edf670a14f3b9a455
Daniel Danis <daniel.danis@jax.org>
2022-01-14 15:06:18 +0100

    Update packaging script
    Signed-off-by: Daniel Danis <daniel.danis@jax.org>

--- package.sh
+++ package.sh
@@ -5,54 +5,121 @@
 # The script works on both UNIX and OSX platforms.
 
 
-SRC_DIR=hpo-case-annotator-gui
+##
+# Static variables
+SRC_DIR=hpo-case-annotator-app
 APP_NAME="Hpo Case Annotator"
 CMD_NAME="hpo-case-annotator"
-VERSION=1.0.15-SNAPSHOT
+VERSION=2.0.0-RC1-SNAPSHOT
+
 BUILD_DIR=${SRC_DIR}/target
-JAR_NAME=hpo-case-annotator-gui-1.0.15-SNAPSHOT.jar
+JAR_NAME="hpo-case-annotator-app-${VERSION}.jar"
 VENDOR="The Monarch Initiative"
 DESCRIPTION="Hpo Case Annotator simplifies curation of published pathogenic human variants."
-COPYRIGHT="Copyright 2021, All rights reserved"
-ICON="${BUILD_DIR}/classes/img/app-icon" # Update icon once we have a better one
-PACKAGE_DIR=/tmp/hca_app
-
-# build
-printf "Building Hpo Case Annotator\n"
-./mvnw clean package -DskipTests
+COPYRIGHT="Copyright 2022, All rights reserved"
+ICON="${BUILD_DIR}/classes/img/donald-duck" # Update icon once we have a better one
 
 
-# prepare packaging folder
-printf "Creating temporary directory at %s\n" ${PACKAGE_DIR}
-mkdir -p $PACKAGE_DIR
-cp $BUILD_DIR/$JAR_NAME $PACKAGE_DIR
-
-# package
 function detect_platform() {
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
-      echo "linux"
+      echo "Linux"
     elif [[ "$OSTYPE" =~ darwin.* ]]; then
-      echo "osx";
+      echo "Osx";
     else
       # More people use OSX
-      echo "osx"
+      echo "Osx"
     fi
 }
 
+function build_for_module_path() {
+    # copy the JAR and lib (dependencies)
+    cp $BUILD_DIR/$JAR_NAME $PACKAGE_DIR
+    cp -r $BUILD_DIR/lib $PACKAGE_DIR
+    if [[ "$PLATFORM" == "Linux" ]]; then
+      # Setup Linux CLI using module path
+#      MODULE="org.monarchinitiative.hpo_case_annotator.app/org.monarchinitiative.hpo_case_annotator.App"
+#      MPATH="${PACKAGE_DIR}/lib:${PACKAGE_DIR}/${JAR_NAME}"
+#      jpackage --name "${APP_NAME}" --module-path "${MPATH}" --module "${MODULE}" --linux-menu-group Science --linux-shortcut --linux-package-name "${CMD_NAME}" --icon "${ICON}.png" --app-version "${VERSION}" --description "${DESCRIPTION}" --vendor "${VENDOR}" --license-file LICENSE --copyright "${COPYRIGHT}"
+      echo "Sorry, module path is not yet supported"
+    elif [[ "$PLATFORM" == "Osx" ]]; then
+      # setup OSX CLI using module path
+      #jpackage --name "${APP_NAME}" --input "${PACKAGE_DIR}" --main-jar "${JAR_NAME}" --mac-package-name "${CMD_NAME}" --icon "${ICON}.icns" --app-version "${VERSION}" --description "${DESCRIPTION}" --vendor "${VENDOR}" --license-file LICENSE --copyright "${COPYRIGHT}"
+      echo "Sorry, module path is not yet supported"
+    else
+      printf "Unknown platform %s\n. Abort." "${PLATFORM}"
+      exit
+    fi
+}
+
+function build_for_class_path() {
+  # copy the fat JAR
+  cp $BUILD_DIR/$JAR_NAME $PACKAGE_DIR
+#  cp $BUILD_DIR/lib/* $PACKAGE_DIR
+  if [[ "$PLATFORM" == "Linux" ]]; then
+    # setup Linux CLI
+    jpackage --name "${APP_NAME}" \
+      --input "${PACKAGE_DIR}" \
+      --main-jar "${JAR_NAME}" \
+      --app-version "${VERSION}" \
+      --icon "${ICON}.png" \
+      --description "${DESCRIPTION}" \
+      --linux-menu-group Science \
+      --linux-shortcut \
+      --linux-package-name "${CMD_NAME}" \
+      --vendor "${VENDOR}" \
+      --license-file LICENSE \
+      --copyright "${COPYRIGHT}"
+#--main-class "org.monarchinitiative.hpo_case_annotator.app.App" \
+  elif [[ "$PLATFORM" == "Osx" ]]; then
+    # setup OSX CLI
+    jpackage --name "${APP_NAME}" \
+    --input "${PACKAGE_DIR}" \
+    --main-jar "${JAR_NAME}" \
+    --app-version "${VERSION}" \
+    --description "${DESCRIPTION}" \
+    --mac-package-name "${CMD_NAME}" \
+    --vendor "${VENDOR}" \
+    --license-file LICENSE \
+    --copyright "${COPYRIGHT}"
+#    --icon "${ICON}.icns" \
+  else
+    printf "Unknown platform %s\n. Abort." "${PLATFORM}"
+    exit
+  fi
+}
+
+########################################################################################################################
+#                                                     PACKAGE
+########################################################################################################################
+
+##
+# Dynamic variables
+#
 PLATFORM=$(detect_platform)
-printf "\nPackaging Hpo Case Annotator for %s\n" "${PLATFORM}"
-if [[ "$PLATFORM" == "linux" ]]; then
-  # setup Linux CLI
-  jpackage --name "${APP_NAME}" --input "${PACKAGE_DIR}" --main-jar "${JAR_NAME}" --linux-menu-group Science --linux-shortcut --icon "${ICON}.png" --linux-package-name "${CMD_NAME}" --app-version "${VERSION}" --description "${DESCRIPTION}" --vendor "${VENDOR}" --license-file LICENSE --copyright "${COPYRIGHT}"
-elif [[ "$PLATFORM" == "osx" ]]; then
-  # setup OSX CLI
-  jpackage --name "${APP_NAME}" --input "${PACKAGE_DIR}" --main-jar "${JAR_NAME}" --mac-package-name "${CMD_NAME}" --icon "${ICON}.icns" --app-version "${VERSION}" --description "${DESCRIPTION}" --vendor "${VENDOR}" --license-file LICENSE --copyright "${COPYRIGHT}"
+PACKAGE="classpath" # or "modular"
+
+
+# 1. Build
+printf "Building Hpo Case Annotator\n"
+./mvnw clean package
+
+# 2. Prepare packaging folder
+PACKAGE_DIR=/tmp/hca_app
+printf "Creating temporary directory at %s\n" ${PACKAGE_DIR}
+mkdir -p $PACKAGE_DIR
+
+# 3. Package for platform and package type
+if [[ "$PACKAGE" == "modular" ]]; then
+  printf "Packaging modular Hpo Case Annotator for %s\n" "${PLATFORM}"
+  $(build_for_module_path)
+elif [[ "$PACKAGE" == "classpath" ]]; then
+  printf "Packaging classpath Hpo Case Annotator for %s\n" "${PLATFORM}"
+  $(build_for_class_path)
 else
-  printf "Unknown platform %s\n. Abort." "${PLATFORM}"
-  exit
+  printf "\nUnknown packaging type '%s'\n" "${PACKAGE}"
 fi
 
-# clean up
+# 4. Clean up the packaging folder
 printf "Removing the temporary directory %s\n" ${PACKAGE_DIR}
 rm -r $PACKAGE_DIR
 printf "Done!\n"
