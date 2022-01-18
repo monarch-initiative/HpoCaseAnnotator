67627805492d17e293eee7d13f35f5ccfafe1af6
Daniel Danis <daniel.danis@jax.org>
2022-01-18 10:44:01 +0100

    Make the `hca.model` module transitive in `hca.test`
    Signed-off-by: Daniel Danis <daniel.danis@jax.org>

--- hpo-case-annotator-test/src/main/java/module-info.java
+++ hpo-case-annotator-test/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module org.monarchinitiative.hca.test {
-    requires org.monarchinitiative.hca.model;
+    requires transitive org.monarchinitiative.hca.model;
 
     exports org.monarchinitiative.hpo_case_annotator.test;
 }
