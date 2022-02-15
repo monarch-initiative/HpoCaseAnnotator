93641b4ab26b63114e4955981cb58e655f778617
Daniel Danis <daniel.danis@jax.org>
2022-02-15 16:02:02 +0100

    Update Javadoc on `Codec`
    Signed-off-by: Daniel Danis <daniel.danis@jax.org>

--- hpo-case-annotator-model/src/main/java/org/monarchinitiative/hpo_case_annotator/model/convert/Codec.java
+++ hpo-case-annotator-model/src/main/java/org/monarchinitiative/hpo_case_annotator/model/convert/Codec.java
@@ -1,9 +1,10 @@
 package org.monarchinitiative.hpo_case_annotator.model.convert;
 
 /**
- * Classes implementing this interface are able to convert instances of {@link SOURCE} to {@link TARGET} format, and back.
+ * Classes implementing this interface are able to convert instances of {@link SOURCE} to {@link TARGET} format.
  *
  * @author <a href="mailto:daniel.danis@jax.org">Daniel Danis</a>
+ * @see BidirectionalCodec for a codec capable of two-way conversion.
  */
 public interface Codec<SOURCE, TARGET> {
 
